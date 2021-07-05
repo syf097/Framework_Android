139bc46d7ebdb714b907044b26f8f26f5ff48c7a
syf097 <2667824422@qq.com>
2021-07-05 12:17:01 +0800

    基础拍照框架

--- app/src/main/java/com/bonait/bnframework/modules/home/fragment/Home1Fragment.java
+++ app/src/main/java/com/bonait/bnframework/modules/home/fragment/Home1Fragment.java
@@ -1,7 +1,16 @@
 package com.bonait.bnframework.modules.home.fragment;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,9 +20,19 @@ import android.view.View;
 
 import com.bonait.bnframework.R;
 import com.bonait.bnframework.common.base.BaseFragment;
-import com.bonait.bnframework.common.utils.ToastUtils;
 import com.orhanobut.logger.Logger;
+import com.qmuiteam.qmui.widget.QMUIRadiusImageView;
 import com.qmuiteam.qmui.widget.QMUITopBar;
+import com.qmuiteam.qmui.widget.dialog.QMUIBottomSheet;
+import com.yalantis.ucrop.UCrop;
+
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -45,9 +64,17 @@ public class Home1Fragment extends BaseFragment {
         initTopBar();
     }
 
-    @OnClick(R.id.button)
-    public void onViewClicked() {
-        ToastUtils.info("主页");
+    @OnClick(R.id.button_pick)
+    public void onClick() {
+        showSimpleBottomSheetList();
+    }
+
+    File mPictureFile =new File("storage/emulated/0/result.jpg");
+    Uri imageUri = Uri.fromFile(mPictureFile);
+
+    @OnClick(R.id.button_change)
+    public void onCh() {
+        startUCrop(imageUri);
     }
 
     private void initTopBar() {
@@ -80,4 +107,128 @@ public class Home1Fragment extends BaseFragment {
     protected boolean canDragBack() {
         return false;
     }
+    private final int CAMERA_REQUEST = 1000;
+    private final int CHOOSE_PHOTO = 1001;
+    private void showSimpleBottomSheetList(){
+        QMUIBottomSheet.BottomListSheetBuilder builder = new QMUIBottomSheet.BottomListSheetBuilder(getActivity());
+        builder.setTitle("")
+                .setOnSheetItemClickListener(new QMUIBottomSheet.BottomListSheetBuilder.OnSheetItemClickListener() {
+                    @Override
+                    public void onClick(QMUIBottomSheet dialog, View itemView, int position, String tag) {
+//                        dialog.dismiss();
+//                        Toast.makeText(getActivity(), "Item " + (position + 1), Toast.LENGTH_SHORT).show();
+                        switch (position) {
+                            case 0:
+                                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//                                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mPictureFile));
+                                startActivityForResult(cameraIntent, CAMERA_REQUEST);break;
+                            case 1:
+                                Intent intent_album = new Intent( Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI );
+
+                                startActivityForResult( intent_album, CHOOSE_PHOTO );
+                        }
+                    }
+                });
+
+        builder.addItem("拍照");
+        builder.addItem("从相册中选择");
+        builder.build().show();
+    }
+    @BindView(R.id.result_img)
+    QMUIRadiusImageView result_img;
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Context context = this.getContext();
+        if ((requestCode == CAMERA_REQUEST) && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            BufferedOutputStream bos = null;
+            try {
+                bos = new BufferedOutputStream(new FileOutputStream(mPictureFile));
+                photo.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+                bos.flush();
+                bos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            result_img.setImageBitmap(photo);
+            imageUri = Uri.fromFile(mPictureFile);
+        }
+        if ((requestCode == CHOOSE_PHOTO) && resultCode == RESULT_OK) {
+            imageUri = data.getData(); //获取系统返回的照片的Uri
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = context.getContentResolver().query(imageUri,
+                    filePathColumn, null, null, null);//从系统表中查询指定Uri对应的照片
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String path = cursor.getString(columnIndex);  //获取照片路径
+            cursor.close();
+            Bitmap bitmap = BitmapFactory.decodeFile(path);
+
+            result_img.setImageBitmap(bitmap);
+        }
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            Uri resultUri = UCrop.getOutput(data);
+            Logger.d(resultUri);
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(context.getContentResolver().openInputStream(resultUri));
+                result_img.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }else if (resultCode == UCrop.RESULT_ERROR) {
+            final Throwable cropError = UCrop.getError(data);
+            cropError.printStackTrace();
+        }
+    }
+    /**
+     * 裁剪图片
+     *
+     * @param sourceUri
+     */
+    private void startUCrop(Uri sourceUri) {
+        UCrop.Options options = new UCrop.Options();
+        //裁剪后图片保存在文件夹中
+        Uri destinationUri = Uri.fromFile(new File("storage/emulated/0/result.jpg"));
+        UCrop uCrop = UCrop.of(sourceUri, destinationUri);//第一个参数是裁剪前的uri,第二个参数是裁剪后的uri
+        uCrop.withAspectRatio(1, 1);//设置裁剪框的宽高比例
+        //下面参数分别是缩放,旋转,裁剪框的比例
+        options.setAllowedGestures(com.yalantis.ucrop.UCropActivity.ALL, com.yalantis.ucrop.UCropActivity.NONE, com.yalantis.ucrop.UCropActivity.ALL);
+        options.setToolbarTitle("移动和缩放");//设置标题栏文字
+        options.setCropGridStrokeWidth(2);//设置裁剪网格线的宽度(我这网格设置不显示，所以没效果)
+        //options.setCropFrameStrokeWidth(1);//设置裁剪框的宽度
+        options.setMaxScaleMultiplier(3);//设置最大缩放比例
+        //options.setHideBottomControls(true);//隐藏下边控制栏
+        options.setShowCropGrid(true);  //设置是否显示裁剪网格
+        //options.setOvalDimmedLayer(true);//设置是否为圆形裁剪框
+        options.setShowCropFrame(true); //设置是否显示裁剪边框(true为方形边框)
+        options.setToolbarWidgetColor(Color.parseColor("#ffffff"));//标题字的颜色以及按钮颜色
+        options.setDimmedLayerColor(Color.parseColor("#AA000000"));//设置裁剪外颜色
+        options.setToolbarColor(Color.parseColor("#000000")); // 设置标题栏颜色
+        options.setStatusBarColor(Color.parseColor("#000000"));//设置状态栏颜色
+        options.setCropGridColor(Color.parseColor("#ffffff"));//设置裁剪网格的颜色
+        options.setCropFrameColor(Color.parseColor("#ffffff"));//设置裁剪框的颜色
+        uCrop.withOptions(options);
+        /*//裁剪后保存到文件中
+        Uri destinationUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/myxmpp/" + "test1.jpg"));
+        UCrop uCrop = UCrop.of(sourceUri, destinationUri);
+        UCrop.Options options = new UCrop.Options();
+        //设置裁剪图片可操作的手势
+        options.setAllowedGestures(UCropActivity.SCALE, UCropActivity.ROTATE, UCropActivity.ALL);
+        //设置toolbar颜色
+        options.setToolbarColor(ActivityCompat.getColor(this, R.color.orange2));
+        //设置状态栏颜色
+        options.setStatusBarColor(ActivityCompat.getColor(this, R.color.orange2));
+        //是否能调整裁剪框
+        options.setFreeStyleCropEnabled(true);
+        options.setToolbarWidgetColor(Color.parseColor("#ffffff"));//标题字的颜色以及按钮颜色
+        options.setDimmedLayerColor(Color.parseColor("#AA000000"));//设置裁剪外颜色
+        options.setToolbarColor(Color.parseColor("#000000")); // 设置标题栏颜色
+        options.setStatusBarColor(Color.parseColor("#000000"));//设置状态栏颜色
+        options.setCropGridColor(Color.parseColor("#ffffff"));//设置裁剪网格的颜色
+        options.setCropFrameColor(Color.parseColor("#ffffff"));//设置裁剪框的颜色
+        //options.setShowCropFrame(false); //设置是否显示裁剪边框(true为方形边框)
+        uCrop.withOptions(options);*/
+        uCrop.start(getContext(),this);
+    }
+
 }
